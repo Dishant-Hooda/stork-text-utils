6729ac33f5d3a717d45d3e6024868d5ca64df3ac
Dishant Hooda <87143136+Dishant-Hooda@users.noreply.github.com>
2025-01-24 23:25:04 +0530

    Add files via upload

--- textutils/settings.py
+++ textutils/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2z&bkug-c)3%^656-72lnm=v-u0xr&guzqzm!8=(975#+**-&&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1']
 
 
 # Application definition
